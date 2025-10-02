9de2dfb5afadd44815fc622d6d834de706be0c63
EdgarRetes <edgar.retes24@gmail.com>
2025-10-01 21:15:22 -0600

    fix type

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 
-const AuthLayout = ({ children }) => {
+const AuthLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
